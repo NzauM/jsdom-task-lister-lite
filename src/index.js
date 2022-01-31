0b7a91ead26d68811cda712d400c90659565af9e
mercy.waeni <mercy.waeni@calltronix.com>
2022-01-31 11:57:57 +0300

    complete task-lister lite lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const submitButton = document.getElementById("submit-btn")
+  submitButton.addEventListener('click', function(e){
+    e.preventDefault()
+    const myTask = document.getElementById("new-task-description")
+    // console.log(myTask)
+    const taskList = document.getElementById("tasks")
+    const listItem = document.createElement("li")
+    listItem.textContent = myTask.value
+    taskList.appendChild(listItem);
+    myTask.value=""
+  })
 });
